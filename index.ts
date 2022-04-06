d43c1378917295bff74384f4f24bb25755b9a35e
Gabriel Eduardo Cunha <61513841+cunha-ambisis@users.noreply.github.com>
2022-04-06 10:36:10 -0300

    final 2.0.0

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
 import query_builder from './query_builder';
-// import data_access_object from './database/data_access_object';
-// import structure_manipulator_object from './database/structure_manipulator_object';
+import DataAccessObject from './data_access_object';
 
 export default {
 	query_builder,
-	// data_access_object,
-	// structure_manipulator_object,
+	DataAccessObject,
 };
