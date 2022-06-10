1eb60f5f3c5e49736632173c741eecdbc755144d
Gabriel Eduardo Cunha <61513841+cunha-ambisis@users.noreply.github.com>
2022-06-10 11:20:56 -0300

    4.0.10
    exporting more useful interfaces and types

--- QueryBuilder/select/columns/types.ts
+++ QueryBuilder/select/columns/types.ts
@@ -5,7 +5,7 @@ export type SelectColumns =
 	| Array<string | SelectObject>
 	| SelectObject;
 
-interface SelectObject {
+export interface SelectObject {
 	[index: number]: never;
 	/**
 	 * Key is the alias. If value type is String will escape the names with \`\`. Name escaping will be ignored if passing an object with expression key containing the query expression.
